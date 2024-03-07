5d315d3dbeed81fc90b2289c490d1ee85789b1ae
Ivan Partida <ivan.97galarz@gmail.com>
2024-03-07 01:03:29 -0800

    chore: remove print statements

--- nautilus/classes/command.py
+++ nautilus/classes/command.py
@@ -11,22 +11,15 @@ class Command:
     action: Callable
 
     def exec(self, args: list[str]):
-        print(args)
         required_args_qty = len([param for param in self.params if param.required])
         total_args_qty = len(self.params)
         significant_args = args[2:]
 
-        print(significant_args)
-
         passed_args_diff = required_args_qty - len(significant_args)
 
-        print(passed_args_diff)
-
         if total_args_qty - required_args_qty < 0:
-            print(total_args_qty, required_args_qty)
             raise ExceedingArgs
         if passed_args_diff > 0:
-            print(self.params)
             raise MissingRequiredArgs(self.params[passed_args_diff - 1])
         else:
             self.action(*significant_args)
